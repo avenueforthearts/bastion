b52850239ab201943876422d7a1b3afd2484b9e4
beau.furrow <beau.furrow@bluemedora.com>
2017-11-04 17:06:10 -0400

    More passes at facebook event parsers.

--- events/management/commands/facebook_event_parser.py
+++ events/management/commands/facebook_event_parser.py
@@ -1,12 +1,32 @@
 from django.core.management.base import BaseCommand
-from events.models import Facebook
+from events.models import Facebook, Event
 from django.conf import settings
-
+import facebook as FacebookSDK
+from events.serializers import EventSerializer
 
 class Command(BaseCommand):
-    help = "Does a thing"
+    help = "Loops through defined facebook URLs and creates events/locations"
 
     def handle(self, *args, **options):
-        # print(settings.FACEBOOK_TOKEN)
+        token = settings.FACEBOOK_TOKEN
+        graph = FacebookSDK.GraphAPI(access_token=token, version='2.7')
         for facebook in Facebook.objects.values():
-            print(facebook)
+            events = self.request_events(facebook['url'], graph)
+            for event in events:
+                self.process_event(event)
+
+    def request_events(self, org_url, graph):
+        resp = graph.request(org_url)
+        id = resp['id']
+        events = graph.request(id + '/events?type=created')
+        return events['data']
+
+    def process_event(self, event):
+        event_serializer = EventSerializer(data=event)
+        print("What is this")
+        if event_serializer.is_valid():
+            print(event)
+            new_event, created = Event.objects.update_or_create(**event)
+            if not created:
+                print("Dafaq")
+
--- events/serializers.py
+++ events/serializers.py
@@ -1,1 +1,8 @@
 # from rest_framework import serializers
+from events.models import Event
+from rest_framework import serializers
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
