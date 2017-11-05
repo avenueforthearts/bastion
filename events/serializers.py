28becd53ab03f29b1582ddda8bdc2771097406e9
beau.furrow <beau.furrow@bluemedora.com>
2017-11-04 22:59:02 -0400

    Created a custom serializer to validate the event data.

--- events/serializers.py
+++ events/serializers.py
@@ -1,8 +1,25 @@
 # from rest_framework import serializers
-from events.models import Event
 from rest_framework import serializers
 
-class EventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = '__all__'
+
+class EventSerializer(serializers.Serializer):
+    id = serializers.CharField(max_length=100)
+    event_name = serializers.CharField(max_length=1000)
+    place_name = serializers.CharField(max_length=1000)
+    start_time = serializers.DateTimeField()
+    end_time = serializers.DateTimeField()
+
+    def validate_id(self, value):
+        return value
+
+    def validate_event_name(self, value):
+        return value
+
+    def validate_place_name(self, value):
+        return value
+
+    def validate_start_time(self, value):
+        return value
+
+    def validate_end_time(self, value):
+        return value
